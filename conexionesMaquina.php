fc099b6a8311df16cfaf9730047c81ecd6ee36a0
Jefferson <noe.chavarry@educa.madrid.org>
2025-06-13 11:08:48 +0200

    AS

--- conexionesMaquina.php
+++ conexionesMaquina.php
@@ -53,17 +53,17 @@ if (!$usuario_id) {
                     <ul class="sidebar-nav position-relative w-100">
                         <li class="sidebar-header">Navegacion</li>
                         <li class="sidebar-item">
-                            <a href="producto.php" class="sidebar-link" data-bs-toggle="collapse" aria-expanded="false">
+                            <a href="producto.php" class="sidebar-link" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-house" viewBox="0 0 16 16">
                                     <path
                                         d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
                                 </svg>
-                                <span class="align-middel"><a href="index.php">Inicio</a></span>
+                                <span class="align-middel">Inicio</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="producto.php" class="sidebar-link" data-bs-toggle="collapse" aria-expanded="false">
+                            <a href="producto.php" class="sidebar-link" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-globe" viewBox="0 0 16 16">
                                     <path
@@ -74,17 +74,17 @@ if (!$usuario_id) {
                         </li>
                         <li class="sidebar-header">Servicios</li>
                         <li class="sidebar-item">
-                            <a href="producto.php" class="sidebar-link" data-bs-toggle="collapse" aria-expanded="false">
+                            <a href="producto.php" class="sidebar-link" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-diagram-3-fill" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd"
                                         d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5zm-6 8A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5zm6 0A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5z" />
                                 </svg>
-                                <span class="align-middel"><a href="conexionesWeb.php">Conexiones</a></span>
+                                <span class="align-middel">Conexiones</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="producto.php" class="sidebar-link" data-bs-toggle="collapse" aria-expanded="false">
+                            <a href="producto.php" class="sidebar-link" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-pc-display" viewBox="0 0 16 16">
                                     <path
@@ -94,28 +94,28 @@ if (!$usuario_id) {
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="producto.php" class="sidebar-link" data-bs-toggle="collapse" aria-expanded="false">
+                            <a href="producto.php" class="sidebar-link" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-pc-display" viewBox="0 0 16 16">
                                     <path
                                         d="M8 1a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1zm1 13.5a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0m2 0a.5.5 0 1 0 1 0 .5.5 0 0 0-1 0M9.5 1a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM9 3.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1h-5a.5.5 0 0 0-.5.5M1.5 2A1.5 1.5 0 0 0 0 3.5v7A1.5 1.5 0 0 0 1.5 12H6v2h-.5a.5.5 0 0 0 0 1H7v-4H1.5a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5H7V2z" />
                                 </svg>
-                                <span class="align-middel"><a href="usuarios.php">Usuarios</a></span>
+                                <span class="align-middel">Usuarios</span>
                             </a>
                         </li>
                         <li class="sidebar-header"><?= htmlspecialchars($nombreUsuario) ?></li>
                         <li class="sidebar-item">
-                            <a href="producto.php" class="sidebar-link" data-bs-toggle="collapse" aria-expanded="false">
+                            <a href="producto.php" class="sidebar-link">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-gear-fill" viewBox="0 0 16 16">
                                     <path
                                         d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
                                 </svg>
-                                <span class="align-middel"><a href="modificaciones.php">Modificaciones</a></span>
+                                <span class="align-middel">Modificaciones</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="producto.php" class="sidebar-link" data-bs-toggle="collapse" aria-expanded="false">
+                            <a href="permisos.php" class="sidebar-link" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-person-gear" viewBox="0 0 16 16">
                                     <path
